f588997b7420c00de5ff49b4ccf351cd1a23728b
wwolny <wwolny1@gmail.com>
2018-05-20 22:34:43 +0200

    FirstTryOnNewBranch

---
+++ src/com/PongElements/Obstacle.java
@@ -0,0 +1,13 @@
+/**
+ * Pong application
+ */
+package com.PongElements;
+
+/**
+ * View element that use Graphics Context to create obstacle at the Scene
+ * @author wojtek
+ *
+ */
+public class Obstacle {
+
+}
